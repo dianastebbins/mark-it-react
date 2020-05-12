13cfbd867719551516d295c6a5c59601a94bb8f4
Yalda Ali Aghazadeh <zahraaliaghazadeh@Yaldas-MacBook-Pro.local>
2020-05-11 23:25:36 -0700

    wip

--- src/pages/UserLandingPage/index.js
+++ src/pages/UserLandingPage/index.js
@@ -53,21 +53,13 @@ export default function UserLandingPage() {
             <h3>Navbar goes here</h3>
             <h3>Notification section...Vendor added product or date, other?</h3>
             <h3>IF(user is Vendor and selected Vendor mgmt)<br></br>
-                ....Schedule at a Market link goes here<br></br>
-                ....Manage Products link goes here<br></br>
-                ....Manage Profile link goes here<br></br>
-                ....View Upcoming Schedule link goes here<br></br>
-                ELSE Favs was selected<br></br>
-                ....Favorite Markets link goes here<br></br>
-                ....Search All Markets link goes here<br></br>
-                ....Favorite Vendors link goes here<br></br>
-                ....Search All Vendors link goes here<br></br>
             </h3>
             <button class="button is-dark">Schedule at a matket</button><br></br>
             <button class="button is-dark">Manage Products</button><br></br>
             <button class="button is-dark">Manage Product Link</button><br></br>
             <button class="button is-dark">Manage Profile Link</button><br></br>
             <button class="button is-dark">View Upcoming schedule</button><br></br>
+            ELSE Favs was selected<br></br>
 
             <button class="button is-primary">Favorite Markets link</button><br></br>
             <button class="button is-primary">Search All Markets link</button><br></br>
