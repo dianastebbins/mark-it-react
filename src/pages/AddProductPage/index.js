dc5a6e7664711e782ef083cf3b250441a308fd4a
Yalda Ali Aghazadeh <zahraaliaghazadeh@Yaldas-MacBook-Pro.local>
2020-05-13 15:19:43 -0700

    wipp

--- src/pages/AddProductPage/index.js
+++ src/pages/AddProductPage/index.js
@@ -19,6 +19,36 @@ export default function AddProductPage() {
         upload_photo: ''
     })
 
+    // start of uploading images
+// dw69fw1u3 is my cloudname
+
+    // https://api.cloudinary.com/v1_1/dw69fw1u3/image/upload
+
+    // uploadFile = async e => {
+    //     const files = e.target.files;
+    //     const data = new FormData();
+
+    //     data.append("file", files[0]);
+    //     // this data/preset is required by cloudinary (named sick fits in the cloudinary settings)
+    //     data.append("upload_preset", "markit");
+    //     const res = await fetch(
+    //         "https://api.cloudinary.com/v1_1/dw69fw1u3/image/upload",
+
+    //         {
+    //             method: "POST",
+    //             body: data
+    //         }
+    //     );
+    //     const file = await res.json();
+    //     // this.setState({
+    //     //     image: file.secure_url,
+    //     //     largeImage: file.eager[0].secure_url
+    //     // });
+    // }; 
+
+    // end of uploading images
+
+
     // useEffect(()=>{
     //     API.getAllPlayers().then(res=>{
     //         console.log(res.data)
@@ -66,11 +96,11 @@ export default function AddProductPage() {
     return (
         <div className="AddProductPage">
 
-<div className="container">
+            <div className="container">
 
 
 
-            <div className="body">
+                {/* <div className="body"> */}
 
 
 
@@ -102,67 +132,111 @@ export default function AddProductPage() {
             </div> */}
                 <div className="section mainSection">
 
-                    <form>
-                        <div className="field is-horizontal">
-                            <div className="field-body">
-                                <div className="field">
-                                    <label className="label">Product Name</label>
-                                    <div className="control">
-                                        <input className="input is-hovered" type="text" onChange={handleInputChange} name="product_name" value={productState.product_name} placeholder="Product Name" />
-                                    </div>
-                                </div>
-                                <div className="field">
-                                    <label className="label">Product Description</label>
-                                    <div className="control">
-                                        <input className="input is-hovered" type="text" onChange={handleInputChange} name="product_description" value={productState.product_description} placeholder="Description" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="field is-horizontal">
-                            <div className="field-body">
-                                <div className="field">
-                                    <label className="label">Price</label>
-                                    <div className="control">
-                                        <input className="input is-hovered" type="text" onChange={handleInputChange} name="price" value={productState.price} placeholder="Price" />
-                                    </div>
-                                </div>
-                                <div className="field">
-                                    <label className="label">Unit</label>
-                                    <div className="control">
-                                        <input className="input is-hovered" type="text" onChange={handleInputChange} name="price_unit" value={productState.unit} placeholder="Unit" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="field is-horizontal">
-                            <div className="field-body">
-                                <div className="field">
-                                    <label className="label">Details</label>
-                                    <div className="control">
-                                        <input className="input is-hovered" type="text" onChange={handleInputChange} name="details" value={productState.details} placeholder="details" />
-                                    </div>
-                                </div>
-                                <div className="field">
-                                    <label className="label">Photo Placeholder</label>
-                                    <div className="control">
-                                        <input className="input is-hovered" type="text" onChange={handleInputChange} name="upload_photo" value={productState.upload_photo} placeholder="use upload component instead" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="box">
 
-                        <button className="button is-link is-outlined is-light" onClick={handleFormSubmit}>Add Product</button>
-                    </form>
-                    {/* <div className="section">
+
+
+                        <form>
+                            <div className="field">
+
+                                <div className="field">
+                                    {/* product name */}
+                                    <div className="field">
+                                        <label className="label">Product Name</label>
+                                        <div className="control">
+                                            <input className="input is-hovered" type="text" onChange={handleInputChange} name="product_name" value={productState.product_name} placeholder="Product Name" />
+                                        </div>
+                                    </div>
+                                    {/* product description */}
+                                    <div className="field">
+                                        <label className="label">Product Description</label>
+                                        <div className="control">
+                                            <input className="input is-hovered" type="text" onChange={handleInputChange} name="product_description" value={productState.product_description} placeholder="Description" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label className="label">Price</label>
+                                        <div className="control">
+                                            <input className="input is-hovered" type="text" onChange={handleInputChange} name="price" value={productState.price} placeholder="Price in USD" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label className="label">Unit</label>
+                                        <div className="control">
+                                            <input className="input is-hovered" type="text" onChange={handleInputChange} name="price_unit" value={productState.unit} placeholder="Unit" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label className="label">Photo Placeholder</label>
+                                        <div className="control">
+                                            <input className="input is-hovered" type="text" onChange={handleInputChange} name="upload_photo" value={productState.upload_photo} placeholder="use upload component instead" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label className="label">Details</label>
+                                        <div className="control">
+                                            <textarea className="textarea is-hovered" type="input" onChange={handleInputChange} name="details" value={productState.details} placeholder="details" />
+                                        </div>
+                                    </div>
+                                    <div class="field">
+                                        <div class="control">
+                                            <label class="checkbox">
+                                                <input type="checkbox" />
+                                            I agree to the <a href="#">terms and conditions</a>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <button className="button is-link is-outlined is-light" onClick={handleFormSubmit}>Add Product</button>
+
+                                </div>
+                            </div>
+                            {/* <div className="field">
+                                <div className="field-body">
+                                    <div className="field">
+                                        <label className="label">Price</label>
+                                        <div className="control">
+                                            <input className="input is-hovered" type="text" onChange={handleInputChange} name="price" value={productState.price} placeholder="Price in US$" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label className="label">Unit</label>
+                                        <div className="control">
+                                            <input className="input is-hovered" type="text" onChange={handleInputChange} name="price_unit" value={productState.unit} placeholder="Unit" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> */}
+                            {/* <div className="field">
+                                <div className="field-body">
+                                    <div className="field">
+                                        <label className="label">Photo Placeholder</label>
+                                        <div className="control">
+                                            <input className="input is-hovered" type="text" onChange={handleInputChange} name="upload_photo" value={productState.upload_photo} placeholder="use upload component instead" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label className="label">Details</label>
+                                        <div className="control">
+                                            <textarea className="textarea is-hovered" type="input" onChange={handleInputChange} name="details" value={productState.details} placeholder="details" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> */}
+
+
+
+                        </form>
+                        {/* <div className="section">
                         <button class="button">
                             <Link to="/detail" >DetailPage</Link>
                         </button>
                     </div> */}
+                        {/* </div> */}
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
 
     )
 }
+
