225c5b6a4f0c85eeb0a1a6f73c8fc737f9e8b741
Huelsgoat <huntsperger@gmail.com>
2020-05-17 18:21:39 -0700

    navbar fix, cond. rendering on userpage, full-width video

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 import { Link, useHistory } from "react-router-dom";
 import API from "../../utils/API"
 import photo from "../../assets/images/brandlogo.png"
 
 export default function Nav(props) {
+  const [isActive, setisActive] = useState(false);
+
   const history = useHistory()
   
   
@@ -20,44 +22,47 @@ export default function Nav(props) {
     <div>
       <nav className="navbar is-dark is-bold" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          <span className="navbar-item" href="#">
-            {/* <i className="brandlogo"><img src="assets/images/screenshot4.png" height="100px" width="90px"></img></i> */}
-          </span>
+          
 
           <a
+            onClick={() => {
+              setisActive(!isActive);
+            }}
             role="button"
-            className="navbar-burger burger"
+            className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
             aria-label="menu"
             aria-expanded="false"
             data-target="navbarBasicExample"
           >
-            <span aria-hidden="true">Home</span>
-            <span aria-hidden="true">About</span>
-            <span aria-hidden="true">Login</span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
           </a>
         </div>
 
-        <div id="navbarBasicExample" className="navbar-menu Nav">
+        <div id="navbarBasicExample" className="navbar-menu"
+        className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+
           <div className="navbar-start">
           <a className=" brandlogo" href="/"><img src={photo} alt="logo" height="80px" width="80px"></img></a>
             {/* <a className="navbar-item" href="/"> Home</a> */}
             <a className="navbar-item" href="/map">Find Markets</a>
             <a className="navbar-item" href="/add-product">Add Product</a>
             <a className="navbar-item" href="/detail">Items for Sale</a>
-
+            {props.currentUser?<a className="navbar-item" href={`/user/${props.id}`}>My Profile</a>:""}
+            <a className="navbar-item" href="/about">About</a>
 
             {/* <a className="navbar-item">Documentation</a> */}
 
-            <div className="navbar-item has-dropdown is-hoverable">
+            {/* <div className="navbar-item has-dropdown is-hoverable">
               <span className="navbar-link">More</span>
 
               <div className="navbar-dropdown">
-              {props.currentUser?<a className="navbar-item" href={`/user/${props.id}`}>My Profile</a>:""}
-                <a className="navbar-item" href="/about">About</a>
+              
                 {/* <span className="navbar-item">Contact</span> */}
                 {/* <hr className="navbar-divider" /> */}
-              </div>
-            </div>
+              {/* </div> */}
+            {/* </div> */} 
           </div>
 
           <div className="navbar-end">
