69b397294e131195475971c095d51d02d967114d
Huelsgoat <huntsperger@gmail.com>
2020-05-20 18:28:32 -0700

    wip

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <footer className="footer">
                 <div className="content has-text-centered">
                     <div color="white">Mark-It</div> by Passionate Developers. The source code is licensed by
-                <a target="_blank" rel="noopener noreferrer" href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+                <a target="_blank" rel="noopener noreferrer" href="http://opensource.org/licenses/mit-license.php"> MIT</a>.
                 </div>
                 <h5 className="copyright">&copy; 2020 Copyright</h5>
 
